67116e05f87aaae945f83df839704da9fddd1a29
Theophilus Adesola <theophilusadesola002@gmail.com>
2026-08-12 08:54:29 +0100

    fix(deploy): rename DeployScript to Deploy to match Makefile

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -5,7 +5,7 @@ import "forge-std/Script.sol";
 import "../src/MockUSDC.sol";
 import "../src/OgaRentFactory.sol";
 
-contract DeployScript is Script {
+contract Deploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
